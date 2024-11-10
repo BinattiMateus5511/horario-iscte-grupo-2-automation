6dca100e1d97ae6074156116d31419f3a70542fa
Mateus <mateustcem@gmail.com>
2024-11-10 20:38:47 +0000

    pra nao perder

--- e2e/upload/upload.spec.ts
+++ e2e/upload/upload.spec.ts
@@ -14,10 +14,22 @@ test.describe('Upload de arquivos', () => {
 
   });
 
-  test('Upload de arquivo com extensao diferente de .csv', async () => {
+  test('Validar o grid de horários carregados', async () =>{
 
-    let fileName = "teste.pdf"
-    let expectedErrorMessage = "Apenas arquivos .csv são permitidos.";
+    await uploadPage.captureTableHeaders();
+
+    const tableHeadersText = await uploadPage.getTableHeaders();
+    const tableHeadersCount = await uploadPage.getTableHeadersCount();
+
+    expect(tableHeadersCount).toBe(4);
+    expect(tableHeadersText).toStrictEqual(['ID', 'Nome do Horário', 'Data de Upload', 'Ações']);
+
+  });
+
+  test('Validar upload de arquivo com extensao diferente de .csv', async () => {
+
+    const fileName = "teste.pdf"
+    const expectedErrorMessage = "Apenas arquivos .csv são permitidos.";
 
     await uploadPage.uploadWrongFile(fileName);
     await uploadPage.captureErrorMessage('.file-error');
@@ -27,12 +39,12 @@ test.describe('Upload de arquivos', () => {
 
   });
 
-  test('Upload de arquivo em branco', async () => {
+  test('Validar upload de arquivo em branco', async () => {
 
-    let fileName = "vazio.csv"
-    let expectedErrorMessage = "O arquivo CSV está vazio e não pode ser processado.";
+    const fileName = "vazio.csv"
+    const expectedErrorMessage = "O arquivo CSV está vazio e não pode ser processado.";
 
-    await uploadPage.uploadFile(fileName);
+    await uploadPage.uploadFile(fileName, 1);
     await uploadPage.captureErrorMessage('.success-message-container');
 
     const consoleMessage = uploadPage.getConsoleMessage();
@@ -45,12 +57,16 @@ test.describe('Upload de arquivos', () => {
 
   });
 
-  test('Upload de arquivo com os horários', async () => {
+  test('Validar upload de arquivo com os horários', async () => {
 
-    let fileName = "ficheiro-horarios-teste.csv"
-    let expectedMessage = "Horários atualizados com sucesso!";
+    const fileName = "ficheiro-horarios-teste.csv";
+    const expectedMessage = "Horários atualizados com sucesso!";
+    const botaoDetalhesLocator = 'Detalhes';
+    const botaoApagarLocator = 'Apagar';
+    const botaoEliminarLocator = 'Eliminar';
+    const botaoCancelarLocator = 'Cancelar';
 
-    await uploadPage.uploadFile(fileName);
+    await uploadPage.uploadFile(fileName, 1);
     await uploadPage.captureErrorMessage('.success-message-container');
 
     const displayedMessage = uploadPage.getErrorMessage();
@@ -58,21 +74,44 @@ test.describe('Upload de arquivos', () => {
 
     await uploadPage.clickButton('Fechar');
 
+    const fileNameElement = await uploadPage.findElement(fileName);
+    const botaoDetalhesElement = await uploadPage.findElement(botaoDetalhesLocator);
+    const botaoApagarElement = await uploadPage.findElement(botaoApagarLocator);
+    
+    await expect(fileNameElement).toBeVisible();
+    await expect(botaoDetalhesElement).toBeVisible();
+    await expect(botaoApagarElement).toBeVisible();
+
+    await uploadPage.clickButton(botaoApagarLocator);
+    await uploadPage.clickButton(botaoCancelarLocator);
+
+    await uploadPage.clickButton(botaoApagarLocator);
+    await uploadPage.clickButton(botaoEliminarLocator);
+
+    await expect(fileNameElement).toHaveCount(0);
+    await expect(botaoDetalhesElement).toHaveCount(0);
+    await expect(botaoApagarElement).toHaveCount(0);
 
   });
 
-  test('Validar o grid de horários carregados', async () =>{
+  test('Validar upload de arquivo de estrutura de salas', async () => {
 
-    await uploadPage.captureTableHeaders();
+    const fileName = "ficheiro-estrutura-salas.csv";
 
-    const tableHeadersText = await uploadPage.getTableHeaders();
-    const tableHeadersCount = await uploadPage.getTableHeadersCount();
+    await uploadPage.clickButton('Alterar Estrutura das Salas');
+    const uploadElement = (await uploadPage.findElementByRoleAndText('button', 'Upload')).nth(1);
+    await expect(uploadElement).toBeDisabled();
+    await uploadPage.clickButton('Fechar');
 
-    expect(tableHeadersCount).toBe(4);
-    expect(tableHeadersText).toBe("[ 'ID', 'Nome do Horário', 'Data de Upload', 'Ações' ]");
+    await uploadPage.clickButton('Alterar Estrutura das Salas');
+    await uploadPage.uploadFile(fileName, 1, 'upload-salas');
+    await uploadElement.click();
+    
+    await uploadPage.captureErrorMessage('.success-message-container');
+    
+    
 
   });
 
-  
 
 });
